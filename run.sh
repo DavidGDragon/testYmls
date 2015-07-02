2f0e1552d0d8594a23a8040719f68ca7369d564f
DavidGDragon <DavidGDragon@users.noreply.github.com>
2015-07-02 16:27:06 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,5 @@
 javac testCrypto.java
 java testCrypto
 
-sudo mv -f local_policy.jar /usr/lib/jvm/java-7-oracle/jre/lib/security
-sudo mv -f US_export_policy.jar /usr/lib/jvm/java-7-oracle/jre/lib/security
-
 java testCrypto
 
