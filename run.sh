af997cfa73abd81f7ad94203d5d54c685875e89b
DavidGDragon <DavidGDragon@users.noreply.github.com>
2015-07-02 16:23:12 -0700

    Force mv
    build java

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-mv local_policy.jar /usr/lib/jvm/java-7-oracle/jre/lib/security
-mv US_export_policy.jar /usr/lib/jvm/java-7-oracle/jre/lib/security
+javac testCrypto.java
+java testCrypto
 
-cd /usr/lib/jvm/java-7-oracle/jre/lib/security
-pwd
-ls
-jrunscript -e 'exit (javax.crypto.Cipher.getMaxAllowedKeyLength("RC5") >= 256);'
+sudo mv -f local_policy.jar /usr/lib/jvm/java-7-oracle/jre/lib/security
+sudo mv -f US_export_policy.jar /usr/lib/jvm/java-7-oracle/jre/lib/security
+
+java testCrypto
+
